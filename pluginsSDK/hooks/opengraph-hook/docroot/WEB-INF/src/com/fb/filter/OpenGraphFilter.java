6dabf14e9cc10af1b0d9109c96de95eaa61f2269
Yari <mahalyari@gmail.com>
2014-11-20 10:58:07 +0100

    Add twitter metadata

--- pluginsSDK/hooks/opengraph-hook/docroot/WEB-INF/src/com/fb/filter/OpenGraphFilter.java
+++ pluginsSDK/hooks/opengraph-hook/docroot/WEB-INF/src/com/fb/filter/OpenGraphFilter.java
@@ -13,19 +13,23 @@
  */
 package com.fb.filter;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.BaseFilter;
-import com.liferay.portal.kernel.servlet.BufferCacheServletResponse;
+import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.servlet.ServletResponseUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.servlet.StringServletResponse;
 import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.HttpUtil;
-import com.liferay.portal.kernel.util.PrefsPropsUtil;
 import com.liferay.portal.kernel.util.StringBundler;
-import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.util.PortalUtil;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
@@ -35,111 +39,82 @@ import javax.servlet.http.HttpServletResponse;
  * @author Julio Camarero
  */
 public class OpenGraphFilter extends BaseFilter {
-	public static final String SKIP_FILTER =
-		OpenGraphFilter.class.getName() + "SKIP_FILTER";
+  
+	public final static String SKIP_FILTER = OpenGraphFilter.class.getName() + "SKIP_FILTER";
+	public final static String OPENGRAPH_XMLNS = "http://ogp.me/ns";
+	public final static String FACEBOOK_XMLNS = "http://ogp.me/ns/fb";
 
-	@Override
-	public boolean isFilterEnabled(
-		HttpServletRequest request, HttpServletResponse response) {
-
-		if (isAlreadyFiltered(request)) {
-			return false;
-		}
-		else {
-			return true;
-		}
-	}
-
-	protected String getContent(HttpServletRequest request, String content) {
-		if (ArrayUtil.isEmpty(_appNamespaces)) {
-			if (_companyId == 0) {
-				_companyId = PortalUtil.getCompanyId(request);
-			}
-
-			try {
-				_appNamespaces = PrefsPropsUtil.getStringArray(
-					_companyId, "APP_NAMESPACES", StringPool.NEW_LINE);
-			}
-			catch (Exception e) {
-				_log.error(e);
-			}
-		}
-
-		if (ArrayUtil.isEmpty(_appNamespaces)) {
-			StringBundler sb = new StringBundler(2 + 4 * _appNamespaces.length);
-
-			sb.append(_START_HEAD);
-			sb.append(" og:http://ogp.me/ns fb:http://ogp.me/ns/fb ");
-
-			for (String appNamespace : _appNamespaces) {
-				sb.append(appNamespace);
-				sb.append(":http://ogp.me/ns/fb/");
-				sb.append(appNamespace);
-				sb.append(StringPool.SPACE);
-			}
-
-			content = StringUtil.replaceFirst(
-				content, _START_HEAD, sb.toString());
-		}
-
-		return content;
-	}
-
-	protected Log getLog() {
-		return _log;
-	}
-
-	protected boolean isAlreadyFiltered(HttpServletRequest request) {
-		if (request.getAttribute(SKIP_FILTER) != null) {
-			return true;
-		}
-		else {
-			return false;
-		}
-	}
-
-	@Override
-	protected void processFilter(
-			HttpServletRequest request, HttpServletResponse response,
-			FilterChain filterChain)
-		throws Exception {
-
-		request.setAttribute(SKIP_FILTER, Boolean.TRUE);
-
-		if (_log.isDebugEnabled()) {
-			String completeURL = HttpUtil.getCompleteURL(request);
-
-			_log.debug("Adding Open Graph Attributes " + completeURL);
-		}
-
-		BufferCacheServletResponse bufferCacheServletResponse =
-			new BufferCacheServletResponse(response);
-
-		processFilter(
-			OpenGraphFilter.class, request, bufferCacheServletResponse,
-			filterChain);
-
-		String content = bufferCacheServletResponse.getString();
-
-		String contentType = response.getContentType();
-
-		if ((contentType != null) &&
-			contentType.startsWith(ContentTypes.TEXT_HTML)) {
-
-			content = getContent(request, content);
-
-			ServletResponseUtil.write(response, content);
-		}
-		else {
-			ServletResponseUtil.write(response, bufferCacheServletResponse);
-		}
-	}
-
-	private static final String _START_HEAD = "<head";
+	private final static String _START_HTML = "<html";
 
 	private static Log _log = LogFactoryUtil.getLog(OpenGraphFilter.class);
 
-	private String[] _appNamespaces = null;
-	private long _companyId = 0;
+	@Override
+	public boolean isFilterEnabled(HttpServletRequest request, HttpServletResponse response) {
+		if (isAlreadyFiltered(request)) {return false;}
+		else {return true;}
+	}
 
+	protected String getContent(HttpServletRequest request, String content) {
+		StringBundler sb = new StringBundler();
+		sb.append(_START_HTML);
+		sb.append(" xmlns:og=\"" + OPENGRAPH_XMLNS + "\" xmlns:fb=\"" + FACEBOOK_XMLNS + "\"");
+		content = StringUtil.replaceFirst(content, _START_HTML, sb.toString());
+		return content;
+	}
+
+	protected Log getLog() {return _log;}
+
+	protected boolean isAlreadyFiltered(HttpServletRequest request) {
+		if (request.getAttribute(SKIP_FILTER) != null) {return true;}
+		else {return false;}
+	}
+
+	@Override
+	protected void processFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws Exception {
+
+		request.setAttribute(SKIP_FILTER, Boolean.TRUE);
+		if (_log.isDebugEnabled()) {
+			String completeURL = HttpUtil.getCompleteURL(request);
+			_log.debug("Adding Open Graph Attributes " + completeURL);
+		}
+
+		StringServletResponse stringServletResponse = new StringServletResponse(response);
+		processFilter(OpenGraphFilter.class, request, stringServletResponse, filterChain);
+		
+		String contentType = response.getContentType();
+		if ((contentType != null)&&(contentType.startsWith(ContentTypes.TEXT_HTML))) {
+			String contentEncoding = stringServletResponse.getHeader(HttpHeaders.CONTENT_ENCODING);
+			boolean isGZIP = ((contentEncoding != null)&&(contentEncoding.toLowerCase().equals("gzip")));
+			String content;
+			if (isGZIP) {
+				content = decompress(stringServletResponse.getUnsyncByteArrayOutputStream().toByteArray(), response.getCharacterEncoding());
+			} else {
+				content = stringServletResponse.getString();
+			}
+			content = getContent(request, content);
+			if (isGZIP) {ServletResponseUtil.write(response, compress(content, response.getCharacterEncoding()));}
+			else {ServletResponseUtil.write(response, content);}
+		} else {
+			ServletResponseUtil.write(response, stringServletResponse);
+		}
+		
+	}
+
+  public static byte[] compress(String str, String encoding) throws Exception {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		GZIPOutputStream gos = new GZIPOutputStream(baos);
+		gos.write(str.getBytes(encoding));
+		gos.close();
+		return baos.toByteArray();
+  }
+
+  public static String decompress(byte[] source, String encoding) throws Exception {
+		GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(source));
+		BufferedReader br = new BufferedReader(new InputStreamReader(gis, encoding));
+		StringBuffer out = new StringBuffer();
+		String line;
+		while ((line = br.readLine()) != null) {out.append(line);}
+		return out.toString();
+  }
+    
 }
