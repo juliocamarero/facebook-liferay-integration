6dbea86c2790de1282d4d441338e553279d11edb
Yari <mahalyari@gmail.com>
2014-11-20 10:58:07 +0100

    Add twitter metadata

--- pluginsSDK/hooks/opengraph-hook/docroot/WEB-INF/src/com/fb/action/OpenGraphLayoutAction.java
+++ pluginsSDK/hooks/opengraph-hook/docroot/WEB-INF/src/com/fb/action/OpenGraphLayoutAction.java
@@ -37,104 +37,93 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class OpenGraphLayoutAction extends BaseStrutsAction {
 
+	public final static String OPENGRAPH_ATTRIBUTE = "LIFERAY_SHARED_OPENGRAPH";
+	public final static String FACEBOOK_ATTRIBUTE = "LIFERAY_SHARED_FACEBOOK";
+	public final static String TWITTER_ATTRIBUTE = "LIFERAY_SHARED_TWITTER";
+	
 	@Override
-	public String execute(
-			StrutsAction originalStrutsAction, HttpServletRequest request,
-			HttpServletResponse response)
-		throws Exception {
-
+	public String execute(StrutsAction originalStrutsAction, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		addOpenGraphProperties(request);
-
 		return originalStrutsAction.execute(request, response);
 	}
 
-	protected void addOpenGraphProperties(HttpServletRequest request)
-		throws PortalException, SystemException {
+	protected void addOpenGraphProperties(HttpServletRequest request) throws PortalException, SystemException {
 
-		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		Map<String, String> opengraphAttributes =
-			(Map<String, String>)request.getAttribute(
-				"LIFERAY_SHARED_OPENGRAPH");
-
-		if (opengraphAttributes == null) {
-			opengraphAttributes = new HashMap<String, String>();
-
-			request.setAttribute(
-				"LIFERAY_SHARED_OPENGRAPH", opengraphAttributes);
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(WebKeys.THEME_DISPLAY);
+		Map<String, String> ogAttrs = (Map<String, String>)request.getAttribute(OPENGRAPH_ATTRIBUTE);
+		if (ogAttrs == null) {
+			ogAttrs = new HashMap<String, String>();
+			request.setAttribute(OPENGRAPH_ATTRIBUTE, ogAttrs);
+		}
+		Map<String, String> fbAttrs = (Map<String, String>)request.getAttribute(FACEBOOK_ATTRIBUTE);
+		if (fbAttrs == null) {
+			fbAttrs = new HashMap<String, String>();
+			request.setAttribute(FACEBOOK_ATTRIBUTE, fbAttrs);
+		}
+		Map<String, String> twAttrs = (Map<String, String>)request.getAttribute(TWITTER_ATTRIBUTE);
+		if (twAttrs == null) {
+			twAttrs = new HashMap<String, String>();
+			request.setAttribute(TWITTER_ATTRIBUTE, twAttrs);
 		}
 
 		UnicodeProperties layoutTypeSettings = null;
 		Layout layout = themeDisplay.getLayout();
 		Group group = themeDisplay.getScopeGroup();
 
-		if (layout != null) {
-			layoutTypeSettings = layout.getTypeSettingsProperties();
-		}
+		if (layout != null) {layoutTypeSettings = layout.getTypeSettingsProperties();}
 
-		String ogTitle = layoutTypeSettings.getProperty("og-title");
 		String ogType = layoutTypeSettings.getProperty("og-type");
+		String ogTitle = layoutTypeSettings.getProperty("og-title");
 		String ogImage = layoutTypeSettings.getProperty("og-image");
-
 		String ogDescription = layoutTypeSettings.getProperty("og-description");
 		String ogVideo = layoutTypeSettings.getProperty("og-video");
 		String ogAudio = layoutTypeSettings.getProperty("og-audio");
-		String ogDeterminer = layoutTypeSettings.getProperty("og-determiner");
 
 		if (Validator.isNull(ogType)) {
-			if (group.isUser()) {
-				ogType = "profile";
-			}
-			else {
-				ogType = "website";
-			}
+			if (group.isUser()) {ogType = "profile";}
+			else {ogType = "website";}
 		}
-
-		opengraphAttributes.put("type", ogType);
-
-		opengraphAttributes.put("url", PortalUtil.getCanonicalURL(
-			PortalUtil.getLayoutFullURL(themeDisplay), themeDisplay, layout));
-
-		if (Validator.isNull(ogTitle)) {
-			ogTitle = layout.getTitle(themeDisplay.getLanguageId());
-		}
-
-		opengraphAttributes.put("title", ogTitle);
-
-		if (Validator.isNull(ogDescription)) {
-			ogDescription = layout.getDescription(themeDisplay.getLanguageId());
-		}
-
-		if (Validator.isNotNull(ogDescription)) {
-			opengraphAttributes.put("description",ogDescription);
-		}
-
-		if (Validator.isNotNull(ogVideo)) {
-			opengraphAttributes.put("video", ogVideo);
-		}
-
-		if (Validator.isNotNull(ogAudio)) {
-			opengraphAttributes.put("audio", ogAudio);
-		}
-
-		if (Validator.isNotNull(ogDeterminer)) {
-			opengraphAttributes.put("determiner", ogDeterminer);
-		}
-
-		opengraphAttributes.put("site_name", group.getDescriptiveName());
-
-		opengraphAttributes.put("locale", themeDisplay.getLanguageId());
-
-		if (Validator.isNull(ogImage) && layout.isIconImage()) {
-			ogImage = themeDisplay.getPathImage() + "/layout_icon?img_id=" +
-				layout.getIconImageId() + "&t=" +
+		ogAttrs.put("type", ogType);
+		if (Validator.isNull(ogTitle)) {ogTitle = layout.getTitle(themeDisplay.getLanguageId());}
+		ogAttrs.put("title", ogTitle);
+		ogAttrs.put("url", PortalUtil.getCanonicalURL(PortalUtil.getLayoutFullURL(themeDisplay), themeDisplay, layout));
+		if (Validator.isNull(ogDescription)) {ogDescription = layout.getDescription(themeDisplay.getLanguageId());}
+		if (Validator.isNotNull(ogDescription)) {ogAttrs.put("description", ogDescription);}
+		ogAttrs.put("site_name", group.getDescriptiveName());
+		ogAttrs.put("locale", themeDisplay.getLanguageId());
+		if (Validator.isNotNull(ogVideo)) {ogAttrs.put("video", ogVideo);}
+		if (Validator.isNotNull(ogAudio)) {ogAttrs.put("audio", ogAudio);}
+		if ((Validator.isNull(ogImage))&&(layout.isIconImage())) {
+			ogImage = themeDisplay.getPathImage() + "/layout_icon?img_id=" + layout.getIconImageId() + "&t=" +
 				WebServerServletTokenUtil.getToken(layout.getIconImageId());
 		}
+		if (Validator.isNotNull(ogImage)) {ogAttrs.put("image", ogImage);}
+		
+		String fbAppId = layoutTypeSettings.getProperty("fb-app_id");
+		String fbAdmins = layoutTypeSettings.getProperty("fb-admins");
 
-		if (Validator.isNotNull(ogImage)) {
-			opengraphAttributes.put("image", ogImage);
-		}
+		if (Validator.isNotNull(fbAppId)) {fbAttrs.put("app_id", fbAppId);}
+		if (Validator.isNotNull(fbAdmins)) {fbAttrs.put("admins", fbAdmins);}
+		
+		String twCard = layoutTypeSettings.getProperty("twitter-card");
+		String twTitle = layoutTypeSettings.getProperty("twitter-title");
+		String twDescription = layoutTypeSettings.getProperty("twitter-description");
+		String twImage = layoutTypeSettings.getProperty("twitter-image");
+		String twSite = layoutTypeSettings.getProperty("twitter-site");
+		String twCreator = layoutTypeSettings.getProperty("twitter-creator");
+		
+		if (Validator.isNull(twCard)) {twCard = "summary";}
+		twAttrs.put("card", twCard);
+		if (Validator.isNull(twTitle)) {twTitle = ogTitle;}
+		if (Validator.isNotNull(twTitle)) {twAttrs.put("title", twTitle);}
+		twAttrs.put("url", PortalUtil.getCanonicalURL(PortalUtil.getLayoutFullURL(themeDisplay), themeDisplay, layout));
+		if (Validator.isNull(twDescription)) {twDescription = ogDescription;}
+		if (Validator.isNotNull(twDescription)) {twAttrs.put("description", twDescription);}
+		if (Validator.isNull(twImage)) {twImage = ogImage;}
+		if (Validator.isNotNull(twImage)) {twAttrs.put("image", twImage);}
+		if (Validator.isNotNull(twSite)) {twAttrs.put("site", twSite);}
+		if (Validator.isNotNull(twCreator)) {twAttrs.put("creator", twCreator);}
+		
 	}
 
 }
